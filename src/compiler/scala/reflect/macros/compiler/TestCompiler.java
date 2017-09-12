8ea9391fe6b383f9ee7ca1d1565dfff1c1aa288f
AvroHy <31510019+AvroHy@users.noreply.github.com>
2017-09-12 16:32:16 +0800

    Update TestCompiler.java

--- src/compiler/scala/reflect/macros/compiler/TestCompiler.java
+++ src/compiler/scala/reflect/macros/compiler/TestCompiler.java
@@ -1,12 +1,8 @@
 package scala.reflect.macros.compiler;
 
 public class TestCompiler {
-    //全部删除，重新定义
-
-    //在github上我也将之前的删掉，并追加这句
-
-    //为什么本地提交没显示
     public static void listDemo() {
         System.out.println("一个方法");
     }
+    
 }
