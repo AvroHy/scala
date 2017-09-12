6fb00221139cd427b20bc577429e4ae069f1efc9
hy <hy7_vold@sina.cn>
2017-09-12 16:11:02 +0800

    修改后本能地提交

--- src/compiler/scala/reflect/macros/compiler/TestCompiler.java
+++ src/compiler/scala/reflect/macros/compiler/TestCompiler.java
@@ -2,7 +2,8 @@ package scala.reflect.macros.compiler;
 
 public class TestCompiler {
 
-    
+    //在github上我选择模拟修改此项目
     //我将第一次提交的部分删除了，并且重新提交这句
+    //这时候想必github有了变动，我pull下来看看
     //在github上我也将之前的删掉，并追加这句
 }
